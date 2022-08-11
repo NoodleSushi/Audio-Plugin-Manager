4a001753cc6560ea4dd4f7f2a4ee03081c8d77c7
NoodleSushi <ytmikesstuffs@gmail.com>
2022-08-11 12:58:13 +0800

    Remove DAW Icons

--- Scripts/Editor/Resources.cs
+++ Scripts/Editor/Resources.cs
@@ -8,8 +8,6 @@ namespace PluginManager.Editor
         public static readonly Texture ICON_FOLDER_CLOSE = GD.Load<Texture>("res://Icons/TreeFolderClose.png");
         public static readonly Texture ICON_FOLDER_OPEN = GD.Load<Texture>("res://Icons/TreeFolderOpen.png");
         public static readonly Texture ICON_NODE = GD.Load<Texture>("res://Icons/Node.svg");
-        public static readonly Texture ICON_LIVE = GD.Load<Texture>("res://Icons/Live.png");
-        public static readonly Texture ICON_FL = GD.Load<Texture>("res://Icons/FL.png");
         public static readonly Texture ICON_BOX_OPEN = GD.Load<Texture>("res://Icons/BoxOpen.svg");
         public static readonly Texture ICON_BOX_CLOSE = GD.Load<Texture>("res://Icons/BoxClose.svg");
         public static readonly Texture ICON_REMOVE = GD.Load<Texture>("res://Icons/Remove.svg");
