ab0e06eb8f09140d13abb477d78d879fdbb844aa
NoodleSushi <ytmikesstuffs@gmail.com>
2022-09-11 22:55:41 +0800

    Create Remove Duplicate Names Tool Menu

--- Scripts/Editor/ToolMenus/ToolsMenu.cs
+++ Scripts/Editor/ToolMenus/ToolsMenu.cs
@@ -1,7 +1,9 @@
 using System.Linq;
+using System.Collections.Generic;
 using PluginManager.Editor.Containers;
 using PluginManager.PluginTree;
 using PluginManager.PluginTree.Components;
+using System;
 
 namespace PluginManager.Editor.ToolMenus
 {
@@ -14,6 +16,8 @@ namespace PluginManager.Editor.ToolMenus
             AddItem(nameof(OnCopyFromReferencePressed));
             AddItem(nameof(OnSortFolderPressed));
             AddItem(nameof(OnRevSortFolderPressed));
+            AddItem(nameof(OnRemoveDuplicates));
+            AddItem(nameof(OnNestedRemoveDuplicates));
             AddItem(nameof(OnDeactivateOptionalsPressed));
         }
 
@@ -59,6 +63,55 @@ namespace PluginManager.Editor.ToolMenus
             treeFolder.SetChildren(
                 treeFolder.Children.OrderByDescending(x => x.GetComponent<Name>()?.NameString ?? "")
             );
+            EditorServer.Instance.RefreshFolderEditor();
+        }
+
+        public void RemoveDuplicates(TreeFolder treeFolder, bool nested = false)
+        {
+            HashSet<string> uniqueNames = new();
+            List<TreeEntity> garbage = new();
+            foreach (TreeEntity child in treeFolder.Children)
+            {
+                if (child.GetComponent<Name>() is not Name nameComp)
+                    return;
+                string name = nameComp.NameString;
+                if (uniqueNames.Contains(name))
+                    garbage.Add(child);
+                else
+                    uniqueNames.Add(name);
+            }
+            garbage.ForEach(x => x.Unparent());
+            garbage.Clear();
+            if (nested)
+            {
+                foreach (TreeFolder subTreeFolder in treeFolder.Children.OfType<TreeFolder>())
+                {
+                    RemoveDuplicates(subTreeFolder, true);
+                }
+            }
+        }
+        
+        [PopupItemAttribute("Remove Duplicate Names")]
+        public void OnRemoveDuplicates()
+        {
+            if (EditorServer.Instance.SelectedTreeEntity is not TreeFolder treeFolder)
+            {
+                WindowContainer.Instance.DisplayError("Tree folder is not selected.");
+                return;
+            }
+            RemoveDuplicates(treeFolder);
+            EditorServer.Instance.RefreshFolderEditor();
+        }
+
+        [PopupItemAttribute("Nested Remove Duplicate Names")]
+        public void OnNestedRemoveDuplicates()
+        {
+            if (EditorServer.Instance.SelectedTreeEntity is not TreeFolder treeFolder)
+            {
+                WindowContainer.Instance.DisplayError("Tree folder is not selected.");
+                return;
+            }
+            RemoveDuplicates(treeFolder, true);
             EditorServer.Instance.RefreshFolderEditor();
         }
 
